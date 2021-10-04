1685cb4cff5f20428cba9f89d063e4edde3b39e4
Kevin Hernandez <kevin.hernandez1802@gmail.com>
2021-10-04 17:11:39 -0500

    done

--- src/Redux/Reducers/pngReducer.js
+++ src/Redux/Reducers/pngReducer.js
@@ -4,31 +4,31 @@ import axios from 'axios'
 
 const initialState = {
     background : [
-        {id:0 , png: '/images/Rectangle0.png', name : 'orange'},
-        {id:1 , png: '/images/Rectangle1.png', name : 'orange'},
-        {id:2 , png: '/images/Rectangle2.png', name : 'orange'},
-        {id:3 , png: '/images/Rectangle3.png', name : 'orange'},
-        {id:4 , png: '/images/Rectangle4.png', name : 'orange'},
-        {id:5 , png: '/images/Rectangle5.png', name : 'orange'},
-        {id:6 , png: '/images/Rectangle6.png', name : 'orange'}
+        {id:0 , png: './images/Rectangle0.png', name : 'orange'},
+        {id:1 , png: './images/Rectangle1.png', name : 'orange-gradient'},
+        {id:2 , png: './images/Rectangle2.png', name : 'purple-gradient'},
+        {id:3 , png: './images/Rectangle3.png', name : 'blue-gradient'},
+        {id:4 , png: '/images/Rectangle4.png', name : 'red-gradient'},
+        {id:5 , png: '/images/Rectangle5.png', name : 'violet-gradient'},
+        {id:6 , png: '/images/Rectangle6.png', name : 'green-gradient'}
     ],
     head:[
-        {id:0 , png: '/images/head0.png', name : 'orange'},
-        {id:1 , png: '/images/head1.png', name : 'orange'},
-        {id:2 , png: '/images/head2.png', name : 'orange'},
-        {id:3 , png: '/images/head3.png', name : 'orange'},
-        {id:4 , png: '/images/head4.png', name : 'orange'},
-        {id:5 , png: '/images/head5.png', name : 'orange'},
-        {id:6 , png: '/images/head6.png', name : 'orange'}
+        {id:0 , png: '/images/head0.png', name : 'head1'},
+        {id:1 , png: '/images/head1.png', name : 'head2'},
+        {id:2 , png: '/images/head2.png', name : 'head3'},
+        {id:3 , png: '/images/head3.png', name : 'head4'},
+        {id:4 , png: '/images/head4.png', name : 'head5'},
+        {id:5 , png: '/images/head5.png', name : 'head6'},
+        {id:6 , png: '/images/head6.png', name : 'head7'}
     ],
     outfit:[
-        {id:0 , png: '/images/outfit0.png', name : 'orange'},
-        {id:1 , png: '/images/outfit1.png', name : 'orange'},
-        {id:2 , png: '/images/outfit2.png', name : 'orange'},
-        {id:3 , png: '/images/outfit3.png', name : 'orange'},
-        {id:4 , png: '/images/outfit4.png', name : 'orange'},
-        {id:5 , png: '/images/outfit5.png', name : 'orange'},
-        {id:6 , png: '/images/outfit6.png', name : 'orange'}
+        {id:0 , png: '/images/outfit0.png', name : 'outfit1'},
+        {id:1 , png: '/images/outfit1.png', name : 'outfit2'},
+        {id:2 , png: '/images/outfit2.png', name : 'outfit3'},
+        // {id:3 , png: '/images/outfit3.png', name : 'orange'},
+        {id:4 , png: '/images/outfit4.png', name : 'outfit4'},
+        {id:5 , png: '/images/outfit5.png', name : 'outfit5'},
+        {id:6 , png: '/images/outfit6.png', name : 'outfit6'}
     ]
  }
 const GET_BACKGROUND = "GET_BACKGROUND";
